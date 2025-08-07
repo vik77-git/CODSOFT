c4ad8c9b46c0d57f0bd407247dba04a1f364d559
vik77-git <vikram14markiv@gmail.com>
2025-08-07 18:04:42 +0530

    Chatbot.py
    A simple chatbot using python with if-else statements, integrated with datetime library.

--- Chatbot.py
+++ Chatbot.py
@@ -1,16 +1,29 @@
+# a simple chat bot with if else statements shows date and time
 import datetime
 print("Hi! I'm a chatbot. Good to see you.")
 while True:
   inp = input("You: ").lower()
   if(inp == "bye" or inp == "exit"):
+    print("It's nice with you. See you again..!")
     break
   elif("hi" in inp or "hello" in inp):
-    printf("Chatbot :  Hello! How can I help with you today")
+    print("Chatbot :  Hello! How can I help with you today")
   elif("how are you" in inp):
-    printf("I'm fine. Hoe do you do?")
+    print("Chatbot : I'm fine. How do you do?")
   elif("fine" in inp or "good" in inp):
-    printf("That's good to hear!")
+    print("Chatbot : That's good to hear!")
+  elif("time" in inp):
+    now = datetime.datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    print("Chatbot : Now the time is : ",current_time)
   elif("date" in inp):
-    time = datetime.datetime.time()
-    now_time = time.strftime("%H:%M:%S")
-    printf(now_time)
+    now = datetime.datetime.now()
+    current_date = now.strftime("%d/%B/%Y")
+    print("Chatbot : Todat's date : ",current_date)
+  elif("day" in inp):
+    now = datetime.datetime.now()
+    current_day = now.strftime("%A")
+    print("Chatbot : Today's day : ",current_day)
+  else:
+    printf("You've entered something that is out of focus. Plese try with something else...")
+    
